f5cd70cd037310c53d7c609c83c53d25acc5e69d
Clancy Rowley <cwrowley@princeton.edu>
2023-08-09 08:47:31 -0400

    Add config file for latexmkrc

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+dirs="bash conda emacs git python tex"
+
 pushd emacs/.emacs.d && make && popd
 [ ! -d ../.emacs.d ] && mkdir ../.emacs.d
-stow --dotfiles --ignore=Makefile bash conda emacs git python
+stow --dotfiles --ignore=Makefile $dirs
 
