9e6ecb2852097e662dbcde5bd4b30146b5083a53
Clancy Rowley <cwrowley@princeton.edu>
2026-01-26 14:04:04 -0500

    Include alternate versions of pyenv.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ dirs="bash conda emacs git python tex"
 
 pushd emacs/.emacs.d && make && popd
 [ ! -d ../.emacs.d ] && mkdir ../.emacs.d
-stow --dotfiles --ignore=Makefile $dirs
+stow --dotfiles --ignore=NO --ignore=Makefile $dirs
 
